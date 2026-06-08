7acbdc796c2a8d54140528dd877e420463cf15a7
Luiz Felipe <105685195+LuizpFelipe@users.noreply.github.com>
2026-06-08 14:47:54 -0300

    Congurações necessarrias para subir a api

--- core/database.py
+++ core/database.py
@@ -1,7 +1,7 @@
 from sqlmodel import create_engine, Session
+import os
 
-
-DATABASE_URL = "postgresql://postgres:123user@localhost:5432/my_api"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
